a995f528814fad4fae0e3bd3c07425e92fdd53e6
Thomas Jebson <ThomasJebbo@gmail.com>
2024-02-12 11:56:16 +0000

    Update GPUtil.py

--- GPUtil/GPUtil.py
+++ GPUtil/GPUtil.py
@@ -41,6 +41,7 @@ import platform
 
 
 __version__ = '1.4.0'
+CREATE_NO_WINDOW = 0x08000000
 
 class GPU:
     def __init__(self, ID, uuid, load, memoryTotal, memoryUsed, memoryFree, driver, gpu_name, serial, display_mode, display_active, temp_gpu):
@@ -65,7 +66,7 @@ def safeFloatCast(strNumber):
         number = float('nan')
     return number
 
-def getGPUs():
+def getGPUs(hide_terminal_pop_up=True):
     if platform.system() == "Windows":
         # If the platform is Windows and nvidia-smi 
         # could not be found from the environment path, 
@@ -78,8 +79,12 @@ def getGPUs():
 	
     # Get ID, processing and memory utilization for all GPUs
     try:
-        p = Popen([nvidia_smi,"--query-gpu=index,uuid,utilization.gpu,memory.total,memory.used,memory.free,driver_version,name,gpu_serial,display_active,display_mode,temperature.gpu", "--format=csv,noheader,nounits"], stdout=PIPE)
-        stdout, stderror = p.communicate()
+        stdout = subprocess.check_output(
+            [
+                nvidia_smi,
+                "--query-gpu=index,uuid,utilization.gpu,memory.total,memory.used,memory.free,driver_version,name,gpu_serial,display_active,display_mode,temperature.gpu",
+                "--format=csv,noheader,nounits"],
+            creationflags=CREATE_NO_WINDOW)
     except:
         return []
     output = stdout.decode('UTF-8')
